e68054a6fcf724373c15f61e7f550af0dae4a91b
Muhamad Gunawan <igunnawan24@gmail.com>
2024-08-06 02:18:10 +0700

    node

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily : {
+        Poppins : ['Poppins']
+      },
+      colors: {
+        'primary': '#121026',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
