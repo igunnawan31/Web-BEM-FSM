104346bc4a063a99a927a6793593f08b97fd6090
MOHAMMAD IZZA HAKIKI <m.izza.hakiki@gmail.com>
2024-08-12 19:59:08 +0700

    menyelesaikan page kabinet

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ module.exports = {
       screens: {
         '2xl' : '1320px',
       },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(40px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 1.5s ease-out forwards',
+      },
     },
   },
   plugins: [],
